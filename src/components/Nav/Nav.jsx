886e5ffe85450e53ad3e6f4b03adf09ea9605f90
diegoayza <diegoayza82@gmail.com>
2025-10-24 01:37:45 -0300

    Primera versión del proyecto

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom"
+import { useCartContext } from "../../context/CartContext/useCartContext"
+
+export const Nav = () => {
+    const {getTotalItems} =useCartContext();
+
+    return <nav>
+        <ul>
+            <li><Link to={"/"}>Home</Link>
+             </li>
+            <li><Link to={"/category/vaso"}>Vaso</Link>
+             </li>
+            <li><Link to={"/category/mochila"}>Mochila</Link>
+             </li>
+            <li>
+                <Link>Carrito</Link>
+                {getTotalItems()>0 && (
+                  <span className="in-cart">{getTotalItems()}</span>)}
+             </li> 
+
+        </ul>
+        
+    </nav>
+}
